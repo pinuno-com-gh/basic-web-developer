0b7e171dda8b1fb8d498ff1513c22f0e959716f3
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-08 16:59:54 +0000

    Operators

---
+++ php tut/Operators/arithmetic.php
@@ -0,0 +1,18 @@
+<?php 
+// variable 1
+$a = 23;
+
+//variable 2
+$b = 32;
+
+// some arithmetic operations on 
+// these two variables
+
+Echo($a + $b), "\n";
+echo($a - $b), "\n";
+echo($a * $b), "\n";
+echo($a / $b), "\n";
+echo($a % $b); "\n";
+
+
+?>
---
+++ php tut/Operators/string.php
@@ -0,0 +1,11 @@
+<?php 
+$a = "php";
+$b = "Programming";
+$c = "Tutorials!!";
+
+ echo $a.$b.$c, "\n";
+ 
+ $a .= $b . $c;
+ echo $a;
+ 
+?>
