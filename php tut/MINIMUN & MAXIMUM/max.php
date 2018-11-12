58b126402d15a808e45226e391a641d4177b5f43
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-12 17:32:25 +0000

    MINIMUN & MAXIMUM

---
+++ php tut/MINIMUN & MAXIMUM/max.php
@@ -0,0 +1,19 @@
+<?php 
+/*php Max() Function
+ * 
+ * The max value in PHP is used to find the numerically maximum value
+ * of a specified value
+ * 
+ */
+
+// eg max(array_values)
+
+    // or
+    
+// max(value1, value2, ...)
+
+echo (max(23, 34, 56, 76));
+
+
+
+?>
---
+++ php tut/MINIMUN & MAXIMUM/minimum.php
@@ -0,0 +1,16 @@
+<?php 
+//PHP Minimum min() function
+
+/*is used to find the lowest value in an array min function can take
+ * several numbers as an argument value among the passed parameters.
+ */
+
+// eg min(array_values)
+
+  //  or
+  
+// min(value1, value2, ...)
+
+echo (min(12, 4, 64, 98, 29));
+
+?>
