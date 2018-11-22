00bbec451ca0f9fb190887a27ea7ab35fc7a6ee9
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-22 10:58:35 +0000

    GuessGame.php

--- assignment-guess_game/GuessGame.php
+++ assignment-guess_game/GuessGame.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
 <html>
-<body>
+<body style="background-color:cyan">
+<div class='header' align="center">
+<h2>GUESSING GAME</h2>
+<p align="center">*************************************************</p>
+<p align=center>***************************************************</p>
+</div>
+
+</br>
+</br>
 
  
- WELCOME <?php echo $_POST["name"]; ?>
-<p>We are ready to play, the computer has generated random 
+ 
+<h3 align=center> WELCOME <?php echo $_POST["name"]; ?> !!!</h3>
+<p align=center>We are ready to play, the computer has generated random 
 numbers from 0-20, guess any number within 0-20 and see if your
  guess is right.
  </p>
+
+<form action="" >
+<h4 align="center">AM GLADE YOU HAVE MADE IT HERE!!!</h4> 
+<input type="number" name="guess"> 
+<input type="submit" name="submit" value="guess">
+</form>
+
+
+
+
+<?php 
+
+ $number = $_POST['guess'];
+ $submit = $_POST['submit'];
+ $rand = mt_rand(0,20);
+?>
+ 
 
 
 
