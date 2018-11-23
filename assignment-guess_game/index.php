f971c3940ee44f044187fea3019c8f9422d9dd66
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-23 20:37:16 +0000

    index.php

---
+++ assignment-guess_game/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title> The Guess Game</title>
+</head>
+
+
+
+<form action="GuessGame.php" method="POST">
+<h3 align="center">Welcome To The Guess Game</h3>
+<h4 align="center">Please Enter your name to Begin :</h4> 
+<input type="text" name="name">
+<input type="submit" value="submit">
+<br/>
+
+</form>
+
+
+</body>
+</html>
