38248b7a1632c5a1a4f4aaf56c5738fdaaa32d9d
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-27 11:33:28 +0000

    rand().php

---
+++ php tut/Functions/rand().php
@@ -0,0 +1,17 @@
+<?php 
+/* the rand()function is an inbuilt in PHP used to generate a random 
+ * number
+ */
+
+
+
+//rand(min,max)
+
+$randomNumber = rand(0,20);
+print_r($randomNumber);
+
+
+
+
+
+?>
