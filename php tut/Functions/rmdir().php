6e6d17b7f1ea12b539b356b5001cb497e22b62c0
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-27 17:19:28 +0000

    rmdir().php

---
+++ php tut/Functions/rmdir().php
@@ -0,0 +1,14 @@
+<?php 
+
+/* The rmdir()function in PHP is an inbuilt function used
+ * to remove empty directory. it is mandatory for the
+ */ 
+
+//rmdir(dirname,context)
+
+mkdir('gfg');
+$dirname = "gfg";
+
+rmdir($dirname);
+
+?>
