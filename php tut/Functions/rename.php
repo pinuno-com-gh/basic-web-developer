381bb9dfc16f4842a2ff091abdcaad90e014e718
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-11-30 17:36:31 +0000

    rename.php

---
+++ php tut/Functions/rename.php
@@ -0,0 +1,20 @@
+<?php 
+/* The rename function is used to rename a file or dirctory
+ */
+
+//rename (oldname,newname,context)
+
+// Old Name Of The file
+$old_name = "gfg.txt" ;
+
+// New Name For The File
+$new_name = "newgfg.txt" ;
+
+// using rename() function to rename the file
+rename( $old_name, $new_name) ;
+
+
+
+
+
+?>
