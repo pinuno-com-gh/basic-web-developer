aaed71bfd4cfeaec8c0c58834c82ca90938c343c
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-12-03 17:38:40 +0000

    jewishtojd.php

---
+++ php tut/Functions/jewishtojd.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*the function jewsihtojd() function is used
+ * to converts a jewsih Date to a julian Day 
+ * count, the function accepts three parameters 
+ * in forma which are $month/$day/$year
+ */
+
+
+// jewishtojd($month, $day, $year)
+
+// use of jewsihtojd() function() 
+
+$jd = jewishtojd(4, 24, 13);
+
+print($jd);
+
+?>
