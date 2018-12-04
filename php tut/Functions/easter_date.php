2477a97320eb14ceb3205aa9b04c5e7f258cd208
Makonne Akyempon <m.akyempon@pinuno.com.gh>
2018-12-04 17:46:09 +0000

    easter_date.php

---
+++ php tut/Functions/easter_date.php
@@ -0,0 +1,12 @@
+<?php 
+/*The easter_date() function is used to returns the easter date in the 
+ * year passed as an argument.
+ */ 
+
+// easter_date ( $year)
+
+echo date("m-d-y" ,easter_date()); 
+
+
+
+?>
