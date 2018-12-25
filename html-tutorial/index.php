a7ce72ed191f730277deccdbebf12df90bfe4155
mjakyempon <m.j.akyempon@pinuno.com/gh>
2018-12-25 15:13:44 +0000

    Contact, in php in html

--- html-tutorial/index.php
+++ html-tutorial/index.php
@@ -20,14 +20,34 @@
   </style>
 </head>
 <body>
-<h1>Welcome</h1>
-<?php
-  	 $dT = date("m/d/y G.i:s", time());
-	 $t = date("j of F Y, \a\\t g.i ", time()); 
-?>
-	<h2>It is: <?php echo "$dT"; ?></h2>
+<div class="container">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="contact.php">CONTACT</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+...
+	<div class="row">
+    		<div class="col-lg-12">
+            <h1>Welcome</h1>
+            <?php
+              	 $dT = date("m/d/y G.i:s", time());
+            	 $t = date("j of F Y, \a\\t g.i ", time()); 
+            ?>
+        	<h2>It is: <?php echo "$dT"; ?></h2>
+    	</div>
+    </div>
 	<div class="footer">
-	<h2>Powered by pinuno</h2>
+		<h2>Powered by pinuno</h2>
 	</div>
+</div>
 </body>
 </html>
