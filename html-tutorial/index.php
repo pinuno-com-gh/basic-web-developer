7761b633b1d02ced5304f853a3d3b9f1d4bd5771
mjakyempon <m.j.akyempon@pinuno.com/gh>
2018-12-25 13:19:43 +0000

    html tutorial from mahonne

---
+++ html-tutorial/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Hello wonderfull people</title>
+</head>
+<body>
+<?php
+     echo "it is";
+	 print( date("m/d/y G.i:s", time()));
+	 print "Today is ";
+	 print( date("j of F Y, \a\\t g.i ", time()));
+?>
+Hello World
+<footer> powered by Pinuno </footer>
+</body>
+</html>
---
+++ html-tutorial/work.php
@@ -0,0 +1,6 @@
+<?php
+     echo "it is\n";
+	 print( date("m/d/y G.i:s<br>\n", time()));
+	 print "Today is \n";
+	 print( date("j of F Y, \a\\t g.i a\n", time()));
+?>
