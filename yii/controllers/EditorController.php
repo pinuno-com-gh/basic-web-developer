5afa656ee25ffabd63ca29e84ea2644a91b744ff
mjakyempon <m.j.akyempon@pinuno.com/gh>
2019-01-23 16:35:04 +0000

    commiting my tutorial

---
+++ yii/controllers/EditorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class EditorController extends \yii\web\Controller
+{
+    public function actionEditor()
+    {
+        return $this->render('editor');
+    }
+
+}
---
+++ yii/views/post/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\post */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'Title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Body')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
