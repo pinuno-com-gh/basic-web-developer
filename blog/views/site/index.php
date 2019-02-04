d491867ed9bb2e828f133ab7b67f831e4d056b25
mjakyempon <m.j.akyempon@pinuno.com/gh>
2019-02-04 20:57:42 +0000

    committing my tutorial

--- blog/views/site/index.php
+++ blog/views/site/index.php
@@ -7,29 +7,6 @@
 
 
 
-<table class="table table-bordered">  
-                     <tr>  
-                          <th>Posts</th>  
-                     </tr>
-					 
-                <?php  
-                //$query = "SELECT * FROM post";  
-                //$result = mysqli_query($connect, $query);  
-                //while($row = mysqli_fetch_array($result))  
-                //{  
-                  //   echo '  
-                    //      <tr>  
-                      //         <td>  
-                        //            <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="200" width="200" class="img-thumnail" />  
-                          //     </td>  
-                          //</tr>  
-                    // ';  
-                //}  
-                ?>
-				
-                </table>  
-				
-	
 
 
 
